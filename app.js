23c53f9a799f8d9931baaec21522d814b56bc397
cbrittnh <catharinebritt@Catharines-Mac-mini.local>
2014-07-19 18:34:31 -0400

    1

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("#button").click(function () {
+        var toAdd = $("input[name=checkListItem]").val();
+        $(".list").append('<div class="item">' + toAdd + '</div>');
+        $("#item").val("");
+    });
+
+    $(document).on('click', '.item', function () {
+        $(this).css({
+            "text-decoration": "line-through",
+            color: "gray"
+        });
+        playDelete();
+    });
+
+    $(document).on('dblclick', '.item', function () {
+        $(this).remove();
+        playDelete();
+    });
+
+	});
+
+function playDelete() {
+    $('#delete-sound')[0].volume = 0.5;
+    $('#delete-sound')[0].load();
+    $('#delete-sound')[0].play();
+
+	}
+
